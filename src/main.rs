993871d391e31b4065577cb0d0c39285e8068d2e
mecastor <mecastor87@gmail.com>
2024-12-18 06:05:38 +0000

    completed descriptor fn

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,31 @@ fn get_descriptors() -> Result<(String, String)> {
     let secp = Secp256k1::new();
     let password = Some("password".to_string());
 
-    // generate private key from mnemonic and password
+    // generate master private key from mnemonic and password
     let mnemonic: GeneratedKey<_, Segwitv0> =
         Mnemonic::generate((MnemonicType::Words12, Language::English)).unwrap();
     let mnemonic = mnemonic.into_key();
     let xkey: ExtendedKey = (mnemonic, password).into_extended_key().unwrap();
     let xprv = xkey.into_xprv(Network::Regtest).unwrap();
 
-    Ok(("Ok".to_string(), "Ok".to_string()))
+    // derive receiver "m/84h/1h/0h/0" and change "m/84h/1h/0h/1" keys
+    let mut keys = Vec::new();
+
+    for path in ["m/84h/1h/0h/0", "m/84h/1h/0h/1"] {
+        let deriv_path = DerivationPath::from_str(path).unwrap();
+        let derived_xprv = &xprv.derive_priv(&secp, &deriv_path).unwrap();
+        let origin = (xprv.fingerprint(&secp), deriv_path);
+        let derived_xprv_desc_key: DescriptorKey<Segwitv0> = derived_xprv
+            .into_descriptor_key(Some(origin), DerivationPath::default())
+            .unwrap();
+
+        if let Secret(key, _, _) = derived_xprv_desc_key {
+            let mut desc = "wpkh".to_string();
+            desc.push_str(&key.to_string());
+            desc.push_str(")");
+            keys.push(desc);
+        }
+    }
+
+    Ok((keys[0].clone(), keys[1].clone()))
 }
